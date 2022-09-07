d7248e033a0bc226c681f2cfc9dd244fc24dbacc
parveen <parveen4900613@example.com>
2022-09-07 14:38:13 +0530

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
     }
       setUser(JSON.parse(localStorage.getItem('profile')))
      
-  },[location, user.token, logout])
+  },[location])
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
